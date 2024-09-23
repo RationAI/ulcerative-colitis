0daed3545b32a4a29a98cc6c2b3c2baa0bd885eb
Adam Kukučka <524905@muni.cz>
2024-09-23 13:40:39 +0200

    feat: preprocessing - tissue masks

---
+++ preprocessing/generate_tissue_masks.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+
+import pyvips
+import ray
+from openslide import PROPERTY_NAME_MPP_X, PROPERTY_NAME_MPP_Y, OpenSlide
+from rationai.masks import (
+    process_items,
+    tissue_mask,
+    write_big_tiff,
+)
+
+from preprocessing.paths import SLIDES_PATH, TISSUE_MASKS_PATH
+
+
+LEVEL = 3
+
+
+@ray.remote
+def process_slide(slide_path: Path) -> None:
+    with OpenSlide(slide_path) as slide:
+        downsample = slide.level_downsamples[LEVEL]
+        xres = 1000 / (float(slide.properties[PROPERTY_NAME_MPP_X]) * downsample)
+        yres = 1000 / (float(slide.properties[PROPERTY_NAME_MPP_Y]) * downsample)
+
+    slide = pyvips.Image.new_from_file(slide_path, page=LEVEL)
+    mask = tissue_mask(slide)
+    mask_path = TISSUE_MASKS_PATH / slide_path.with_suffix(".tiff").name
+    mask_path.parent.mkdir(exist_ok=True, parents=True)
+    write_big_tiff(mask, path=mask_path, xres=xres, yres=yres)
+
+
+def main() -> None:
+    slides = SLIDES_PATH.rglob("*.tiff")
+    process_items(list(slides), process_item=process_slide)
+
+
+if __name__ == "__main__":
+    main()
---
+++ preprocessing/paths.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+
+SLIDES_PATH = Path("/mnt/data/Projects/IKEM/ulcerative_colitis_test_cohort/tiff/")
+DATAFRAME_PATH = SLIDES_PATH.parent / "IBD_AI_test_Fabian.csv"
+TISSUE_MASKS_PATH = Path("data/tissue_masks")
