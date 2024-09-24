451f87602766e1df3698cbd05320cf784aea662a
Adam Kukučka <524905@muni.cz>
2024-09-24 12:28:59 +0200

    feat: modeling - vgg16 backbone, classification and regression heads

---
+++ ulcerative_colitis/modeling/__init__.py
@@ -0,0 +1,5 @@
+from ulcerative_colitis.modeling.classification_head import ClassificationHead
+from ulcerative_colitis.modeling.regression_head import RegressionHead
+
+
+__all__ = ["ClassificationHead", "RegressionHead"]
---
+++ ulcerative_colitis/modeling/backbone/__init__.py
@@ -0,0 +1,4 @@
+from ulcerative_colitis.modeling.backbone.vgg16 import vgg16
+
+
+__all__ = ["vgg16"]
