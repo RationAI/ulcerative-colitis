b40a5fdc16e900ce315b8fe8cc4b8a3cafd35800
Adam Kukučka <524905@muni.cz>
2024-10-09 21:50:11 +0200

    feat: ordinal regression

---
+++ ulcerative_colitis/loss/__init__.py
@@ -0,0 +1,4 @@
+from ulcerative_colitis.loss.cumulative_link_loss import CumulativeLinkLoss
+
+
+__all__ = ["CumulativeLinkLoss"]
--- ulcerative_colitis/modeling/__init__.py
+++ ulcerative_colitis/modeling/__init__.py
@@ -1,5 +1,6 @@
 from ulcerative_colitis.modeling.classification_head import ClassificationHead
+from ulcerative_colitis.modeling.ordinal_regression_head import LogisticCumulativeLink
 from ulcerative_colitis.modeling.regression_head import RegressionHead
 
 
-__all__ = ["ClassificationHead", "RegressionHead"]
+__all__ = ["ClassificationHead", "RegressionHead", "LogisticCumulativeLink"]
