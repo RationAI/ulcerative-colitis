52bcf00c12fdcae180f72b5897644dab092a6d27
Adam Kukučka <524905@muni.cz>
2024-10-16 21:14:23 +0200

    feat: change generating of cutpoints

--- ulcerative_colitis/modeling/ordinal_regression_head.py
+++ ulcerative_colitis/modeling/ordinal_regression_head.py
@@ -1,3 +1,5 @@
+from typing import Literal
+
 import torch
 from torch import nn
 
@@ -13,27 +15,36 @@ class LogisticCumulativeLink(nn.Module):
     ----------
     num_classes : int
         Number of ordered classes to partition the odds into.
-    init_cutpoints : str (default='ordered')
+    init_cutpoints : Literal["ordered", "random"], default="ordered"
         How to initialize the cutpoints of the model. Valid values are
         - ordered : cutpoints are initialized to halfway between each class.
         - random : cutpoints are initialized with random values.
+    step_cutpoints : int, default=4
+        The step size between cutpoints when `init_cutpoints` is "ordered".
     """
 
-    def __init__(self, num_classes: int, init_cutpoints: str = "ordered") -> None:
+    def __init__(
+        self,
+        num_classes: int,
+        init_cutpoints: Literal["ordered", "random"] = "ordered",
+        step_cutpoints: int = 4,
+    ) -> None:
         assert num_classes > 2, "Only use this model if you have 3 or more classes"
         super().__init__()
 
         self.num_classes = num_classes
         self.init_cutpoints = init_cutpoints
-        if init_cutpoints == "ordered":
-            num_cutpoints = self.num_classes - 1
-            cutpoints = torch.arange(num_cutpoints).float() - num_cutpoints / 2
-            self.cutpoints = nn.Parameter(cutpoints)
-        elif init_cutpoints == "random":
-            cutpoints = torch.rand(self.num_classes - 1).sort()[0]
-            self.cutpoints = nn.Parameter(cutpoints)
-        else:
-            raise ValueError(f"{init_cutpoints} is not a valid init_cutpoints type")
+        match init_cutpoints:
+            case "ordered":
+                num_cutpoints = self.num_classes - 1
+                cutpoints = torch.arange(num_cutpoints).float() * step_cutpoints
+                cutpoints -= (num_cutpoints - 1) * step_cutpoints / 2
+                self.cutpoints = nn.Parameter(cutpoints)
+            case "random":
+                cutpoints = torch.rand(self.num_classes - 1).sort()[0]
+                self.cutpoints = nn.Parameter(cutpoints)
+            case _:
+                raise ValueError(f"{init_cutpoints} is not a valid init_cutpoints type")
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """Converts a single number to probabilities of belonging to each class.
--- ulcerative_colitis/ulcerative_colitis_model.py
+++ ulcerative_colitis/ulcerative_colitis_model.py
@@ -75,4 +75,4 @@ class UlcerativeColitisModel(LightningModule):
         return self(inputs)
 
     def configure_optimizers(self) -> Optimizer:
-        return Adam(self.parameters(), lr=0.0001)
+        return Adam(self.parameters(), lr=0.00001)
