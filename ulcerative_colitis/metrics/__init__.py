6c324148afc6246bc5b94313a0a4091822707f8f
Adam Kukučka <524905@muni.cz>
2024-10-21 18:26:19 +0200

    feat: new metrics

---
+++ ulcerative_colitis/metrics/__init__.py
@@ -0,0 +1,4 @@
+from ulcerative_colitis.metrics.aggregate_metric import MetricAggregator
+
+
+__all__ = ["MetricAggregator"]
--- ulcerative_colitis/ulcerative_colitis_model.py
+++ ulcerative_colitis/ulcerative_colitis_model.py
@@ -1,4 +1,7 @@
+from typing import cast
+
 from lightning import LightningModule
+from rationai.mlkit.lightning.loggers import MLFlowLogger
 from rationai.mlkit.metrics import LazyMetricDict
 from torch import Tensor, nn
 from torch.optim.adam import Adam
@@ -6,9 +9,10 @@ from torch.optim.optimizer import Optimizer
 from torchmetrics import AUROC, Accuracy, MetricCollection, Precision, Recall
 
 from ulcerative_colitis.loss import CumulativeLinkLoss
+from ulcerative_colitis.metrics import MetricAggregator
 from ulcerative_colitis.modeling import LogisticCumulativeLink
 from ulcerative_colitis.modeling.regression_head import RegressionHead
-from ulcerative_colitis.typing import Input, Output
+from ulcerative_colitis.typing import Input, MetadataBatch, Output
 
 
 class UlcerativeColitisModel(LightningModule):
@@ -20,29 +24,51 @@ class UlcerativeColitisModel(LightningModule):
         self.cumulative_link = LogisticCumulativeLink(num_classes=self.num_classes)
         self.criterion = CumulativeLinkLoss()
 
-        self.val_metrics = MetricCollection(
-            {
-                "AUC": AUROC("multiclass", num_classes=self.num_classes, average=None),
-                "accuracy": Accuracy("multiclass", num_classes=self.num_classes),
-                "precision": Precision(
-                    "multiclass", num_classes=self.num_classes, average=None
-                ),
-                "recall": Recall(
-                    "multiclass", num_classes=self.num_classes, average=None
-                ),
-            },
-            prefix="validation/",
+        metrics = {
+            "AUC": AUROC("multiclass", num_classes=self.num_classes, average=None),
+            "accuracy": Accuracy("multiclass", num_classes=self.num_classes),
+            "precision": Precision(
+                "multiclass", num_classes=self.num_classes, average=None
+            ),
+            "recall": Recall("multiclass", num_classes=self.num_classes, average=None),
+        }
+
+        self.val_metrics: dict[str, MetricCollection] = {
+            "tiles_all": MetricCollection(metrics, prefix="validation/tiles/"),
+            "scene_max": MetricAggregator(
+                metrics, agg="max", prefix="validation/scenes_max/"
+            ),
+            "slide_max": MetricAggregator(
+                metrics, agg="max", prefix="validation/slides_max/"
+            ),
+            "scene_mean": MetricAggregator(
+                metrics, agg="mean", prefix="validation/scenes_mean/"
+            ),
+            "slide_mean": MetricAggregator(
+                metrics, agg="mean", prefix="validation/slides_mean/"
+            ),
+        }
+
+        self.test_metrics: dict[str, MetricCollection] = {
+            name: metric.clone(
+                prefix=cast(str, metric.prefix).replace("validation", "test")
+            )
+            for name, metric in self.val_metrics.items()
+        }
+        self.test_metrics["tiles_scene"] = LazyMetricDict(
+            self.test_metrics["tiles_all"].clone(prefix="test/tiles/")
+        )
+        self.test_metrics["tiles_slide"] = LazyMetricDict(
+            self.test_metrics["tiles_all"].clone(prefix="test/tiles/")
         )
 
-        self.test_metrics = LazyMetricDict(self.val_metrics.clone(prefix="test/"))
-
-    def forward(self, x: Tensor) -> Output:
+    def forward(self, x: Tensor) -> Output:  # pylint: disable=arguments-differ
         x = self.backbone(x)
         x = self.decode_head(x)
         x = self.cumulative_link(x)
         return x
 
-    def training_step(self, batch: Input) -> Tensor:
+    def training_step(self, batch: Input) -> Tensor:  # pylint: disable=arguments-differ
         inputs, targets, _ = batch
         outputs = self(inputs)
 
@@ -51,28 +77,59 @@ class UlcerativeColitisModel(LightningModule):
 
         return loss
 
-    def validation_step(self, batch: Input) -> None:
-        inputs, targets, _ = batch
+    def validation_step(self, batch: Input) -> None:  # pylint: disable=arguments-differ
+        inputs, targets, metadata = batch
         outputs = self(inputs)
 
         loss = self.criterion(outputs, targets)
         self.log("validation/loss", loss, on_epoch=True, prog_bar=True)
 
-        self.val_metrics.update(outputs, targets.argmax(dim=1))
-        self.log_dict(self.val_metrics, on_epoch=True)
+        self._update_metrics(self.val_metrics, outputs, targets, metadata)
 
-    def test_step(self, batch: Input) -> None:
+    def on_validation_epoch_end(self) -> None:
+        self._log_metrics(self.val_metrics)
+
+    def test_step(self, batch: Input) -> None:  # pylint: disable=arguments-differ
         inputs, targets, metadata = batch
         outputs = self(inputs)
-        for output, target, slide in zip(
-            outputs, targets, metadata["slide"], strict=False
-        ):
-            self.test_metrics.update(output, target.argmax(dim=1), key=slide)
-        self.log_dict(self.test_metrics, on_epoch=True)
 
-    def predict_step(self, batch: Input) -> Output:
+        self._update_metrics(self.test_metrics, outputs, targets, metadata)
+
+    def on_test_epoch_end(self) -> None:
+        self._log_metrics(self.test_metrics)
+
+    def predict_step(self, batch: Input) -> Output:  # pylint: disable=arguments-differ
         inputs, _, _ = batch
         return self(inputs)
 
     def configure_optimizers(self) -> Optimizer:
         return Adam(self.parameters(), lr=0.00001)
+
+    def _update_metrics(
+        self,
+        metrics: dict[str, MetricCollection],
+        outputs: Tensor,
+        targets: Tensor,
+        metadata: MetadataBatch,
+    ) -> None:
+        metrics["tiles_all"].update(outputs, targets)
+        for output, target, scene in zip(
+            outputs, targets, metadata["slide"], strict=True
+        ):
+            slide = scene.split("_scene_")[0]
+
+            metrics["tiles_scene"].update(output, target, key=scene)
+            metrics["scene_max"].update(output, target, key=scene)
+            metrics["scene_mean"].update(output, target, key=scene)
+
+            metrics["tiles_slide"].update(output, target, key=slide)
+            metrics["slide_max"].update(output, target, key=slide)
+            metrics["slide_mean"].update(output, target, key=slide)
+
+    def _log_metrics(self, metrics: dict[str, MetricCollection]) -> None:
+        for name, metric in metrics.items():
+            if isinstance(metric, LazyMetricDict):
+                assert isinstance(self.logger, MLFlowLogger)
+                self.logger.log_table(metric.compute(), f"{name}.parquet")
+            else:
+                self.log_dict(metric, on_epoch=True)
