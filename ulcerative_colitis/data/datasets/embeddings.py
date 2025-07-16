ee652ffd94f5a4689c5620cd182400c8b648c4e2
Adam Kukučka <524905@mail.muni.cz>
2025-07-16 18:32:57 +0000

    feat: h5 support in dataset

--- ulcerative_colitis/data/datasets/embeddings.py
+++ ulcerative_colitis/data/datasets/embeddings.py
@@ -1,14 +1,15 @@
-from collections.abc import Sequence
+from collections.abc import Iterable, Sequence
 from enum import Enum
 from pathlib import Path
-from typing import Generic, TypeVar, cast
+from typing import Generic, TypeVar
 
+import h5py
 import mlflow
 import mlflow.artifacts
-import numpy as np
 import pandas as pd
 import torch
-from torch.utils.data import Dataset, Subset
+from rationai.mlkit.data.datasets import MetaTiledSlides
+from torch.utils.data import ConcatDataset, Dataset
 
 from ulcerative_colitis.typing import MetadataMIL, MILPredictSample, MILSample
 
@@ -20,140 +21,150 @@ class EmbeddingsMode(Enum):
     NEUTROPHILS = "neutrophils"
     NANCY_HIGH = "nancy_high"
     NANCY_LOW = "nancy_low"
-    NANCY_MIX = "nancy_mix"
 
 
-class _Embeddings(Dataset[T], Generic[T]):
+class Embeddings(MetaTiledSlides[MILSample]):
     def __init__(
         self,
         uri: str,
         uri_embeddings: str,
         mode: EmbeddingsMode | str,
         minimum_region_size: int = 100,
-        folder: str | None = None,
-        slide_names: list[str] | None = None,
+        folder_embeddings: str | None = None,
+    ) -> None:
+        if folder_embeddings is not None:
+            self.folder_embeddings = Path(folder_embeddings)
+        if not self.folder_embeddings.exists():
+            self.folder_embeddings = Path(
+                mlflow.artifacts.download_artifacts(uri_embeddings)
+            )
+
+        self.mode = mode
+        self.minimum_region_size = minimum_region_size
+        super().__init__(uris=[uri])
+
+    def generate_datasets(self) -> Iterable[Dataset[MILSample]]:
+        self.slides = process_slides(self.slides)
+        return [
+            EmbeddingsSlideBags(
+                slide_metadata=slide,
+                tiles=self.filter_tiles_by_slide(slide["id"]),
+                embeddings_file=(self.folder_embeddings / slide["name"]).with_suffix(
+                    ".h5"
+                ),
+                mode=self.mode,
+                minimum_region_size=self.minimum_region_size,
+                include_labels=True,
+            )
+            for _, slide in self.slides.iterrows()
+        ]
+
+
+class EmbeddingsPredict(MetaTiledSlides[MILPredictSample]):
+    def __init__(
+        self,
+        uri: str,
+        uri_embeddings: str,
+        mode: EmbeddingsMode | str,
+        minimum_region_size: int = 100,
+        folder_embeddings: str | None = None,
+    ) -> None:
+        if folder_embeddings is not None:
+            self.folder_embeddings = Path(folder_embeddings)
+        if not self.folder_embeddings.exists():
+            self.folder_embeddings = Path(
+                mlflow.artifacts.download_artifacts(uri_embeddings)
+            )
+
+        self.mode = mode
+        self.minimum_region_size = minimum_region_size
+        super().__init__(uris=[uri])
+
+    def generate_datasets(self) -> Iterable[Dataset[MILSample]]:
+        self.slides = process_slides(self.slides)
+        return [
+            EmbeddingsSlideBags(
+                slide_metadata=slide,
+                tiles=self.filter_tiles_by_slide(slide["id"]),
+                embeddings_file=(self.folder_embeddings / slide["name"]).with_suffix(
+                    ".h5"
+                ),
+                mode=self.mode,
+                minimum_region_size=self.minimum_region_size,
+                include_labels=False,
+            )
+            for _, slide in self.slides.iterrows()
+        ]
+
+
+class EmbeddingsSlideBags(Dataset[T], Generic[T]):
+    def __init__(
+        self,
+        slide_metadata: pd.Series,
+        tiles: pd.DataFrame,
+        file_embeddings: Path,
+        mode: EmbeddingsMode | str,
+        minimum_region_size: int = 100,
         include_labels: bool = True,
     ) -> None:
-        self.mode = EmbeddingsMode(mode)
-        artifacts = Path(mlflow.artifacts.download_artifacts(uri))
-        self.tiles = pd.read_parquet(artifacts / "tiles.parquet")
-        self.slides = pd.read_parquet(artifacts / "slides.parquet")
-        self.slides = process_slides(self.slides, self.mode, slide_names)
-
-        if folder is not None:
-            self.folder = Path(folder)
-        if not self.folder.exists():
-            self.folder = Path(mlflow.artifacts.download_artifacts(uri_embeddings))
-
-        self.minimum_region_size = minimum_region_size
+        self.slide_metadata = slide_metadata
+        self.tiles = tiles.reset_index(drop=True)
+        self.file_embeddings = file_embeddings
+        self.mode = mode
         self.include_labels = include_labels
-        self.bags = self._create_bags(minimum_region_size)
+        self.bag = self._create_bags(minimum_region_size)
 
     def _create_bags(self, minimum_region_size: int) -> pd.DataFrame:
-        bags = self.tiles.groupby(["slide_id", "region"]).agg("size")
+        bags = self.tiles.groupby("region").agg("size")
         return bags[bags >= minimum_region_size].reset_index()
 
     def __len__(self) -> int:
         return len(self.bags)
 
     def __getitem__(self, idx: int) -> MILSample | MILPredictSample:
-        slide_id = self.bags.iloc[idx]["slide_id"]
         region = self.bags.iloc[idx]["region"]
+        region_tiles = self.tiles.query(f"region == {region}")
 
-        slide_metadata = self.slides.query(f"id == {slide_id!s}").iloc[0]
-        slide_tiles = self.tiles.query(f"slide_id == {slide_id!s}").reset_index(
-            drop=True
-        )
-        region_tiles = slide_tiles.query(f"region == {region}")
-        slide_name = get_slide_name(slide_metadata)
-        embeddings = cast(
-            "torch.Tensor",
-            torch.load(
-                (self.folder / slide_name).with_suffix(".pt"), map_location="cpu"
-            ),
-        )
-
-        embeddings = embeddings[region_tiles.index.to_numpy()]
+        with h5py.File(self.file_embeddings, "r") as f:
+            region_embeddings = torch.from_numpy(
+                f["embeddings"][region_tiles.index.to_numpy()]
+            )
 
         metadata = MetadataMIL(
-            slide=slide_name,
-            slide_path=Path(slide_metadata["path"]),
-            level=slide_metadata["level"],
-            tile_extent_x=slide_metadata["tile_extent_x"],
-            tile_extent_y=slide_metadata["tile_extent_y"],
+            slide=self.slide_metadata["name"],
+            slide_path=Path(self.slide_metadata["path"]),
+            level=self.slide_metadata["level"],
+            tile_extent_x=self.slide_metadata["tile_extent_x"],
+            tile_extent_y=self.slide_metadata["tile_extent_y"],
             tiles=region_tiles,
             x=torch.from_numpy(region_tiles["x"].to_numpy()),
             y=torch.from_numpy(region_tiles["y"].to_numpy()),
         )
 
         if not self.include_labels:
-            return embeddings, metadata
+            return region_embeddings, metadata
 
-        label = get_label(slide_metadata, self.mode)
+        label = get_label(self.slide_metadata, self.mode)
 
-        return embeddings, label, metadata
-
-
-class Embeddings(_Embeddings[MILSample]):
-    def __init__(
-        self,
-        uri: str,
-        uri_embeddings: str,
-        mode: EmbeddingsMode | str,
-        minimum_region_size: int = 100,
-        folder: str | None = None,
-        slide_names: list[str] | None = None,
-    ) -> None:
-        super().__init__(
-            uri=uri,
-            uri_embeddings=uri_embeddings,
-            mode=mode,
-            minimum_region_size=minimum_region_size,
-            folder=folder,
-            slide_names=slide_names,
-            include_labels=True,
-        )
+        return region_embeddings, label, metadata
 
 
-class EmbeddingsPredict(_Embeddings[MILPredictSample]):
-    def __init__(
-        self,
-        uri: str,
-        uri_embeddings: str,
-        mode: EmbeddingsMode | str,
-        minimum_region_size: int = 100,
-        folder: str | None = None,
-        slide_names: list[str] | None = None,
-    ) -> None:
-        super().__init__(
-            uri=uri,
-            uri_embeddings=uri_embeddings,
-            mode=mode,
-            minimum_region_size=minimum_region_size,
-            folder=folder,
-            slide_names=slide_names,
-            include_labels=False,
-        )
+class EmbeddingsSubset(ConcatDataset):
+    def __init__(self, datasets: list[Dataset], slides: pd.DataFrame) -> None:
+        super().__init__(datasets)
+        self.slides = slides
 
 
-class EmbeddingsSubset(Subset[MILSample]):
-    def __init__(
-        self,
-        dataset: Embeddings,
-        slide_indices: Sequence[int],
-    ) -> None:
-        self.slides = dataset.slides.iloc[list(slide_indices)].reset_index()
-        bag_indices = np.flatnonzero(dataset.bags["slide_id"].isin(self.slides["id"]))
-        super().__init__(dataset, bag_indices.tolist())
+def create_embeddings_subset(
+    dataset: Embeddings,
+    slide_indices: Sequence[int],
+) -> EmbeddingsSubset:
+    selected_datasets = [dataset.datasets[i] for i in slide_indices]
+    subset_slides = dataset.slides.iloc[list(slide_indices)].reset_index(drop=True)
+    return EmbeddingsSubset(selected_datasets, subset_slides)
 
 
-def get_slide_name(slide_metadata: pd.Series) -> str:
-    return Path(slide_metadata["path"]).stem
-
-
-def process_slides(
-    slides: pd.DataFrame, mode: EmbeddingsMode, slide_names: list[str] | None = None
-) -> pd.DataFrame:
+def process_slides(slides: pd.DataFrame, mode: EmbeddingsMode) -> pd.DataFrame:
     match mode:
         case EmbeddingsMode.NEUTROPHILS:
             slides["neutrophils"] = slides["nancy_index"] >= 2
@@ -163,12 +174,8 @@ def process_slides(
             slides = slides[slides["nancy_index"] >= 2]
             slides["ulceration"] = slides["nancy_index"] == 4
             slides["nancy_index"] -= 2
-        case EmbeddingsMode.NANCY_MIX:
-            slides = slides[slides["nancy_index"] < 4]
-            slides["nancy_mix"] = slides["nancy_index"].isin([1, 3])
 
-    if slide_names is not None:
-        slides = slides[slides["path"].str.contains("|".join(slide_names))]
+    slides["name"] = slides["path"].apply(lambda x: Path(x).stem)
     return slides
 
 
@@ -180,6 +187,3 @@ def get_label(slide_metadata: pd.Series, mode: EmbeddingsMode) -> torch.Tensor:
             return torch.tensor(slide_metadata["nancy_index"]).float()
         case EmbeddingsMode.NANCY_HIGH:
             return torch.tensor(slide_metadata["ulceration"]).float()
-            # return torch.tensor(slide_metadata["nancy_index"]).long()
-        case EmbeddingsMode.NANCY_MIX:
-            return torch.tensor(slide_metadata["nancy_mix"]).float()
