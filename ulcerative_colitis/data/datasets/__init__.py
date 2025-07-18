5b554fa8b8e8c8f66c1fc9b2ca1bb603208711ba
Adam Kukučka <524905@mail.muni.cz>
2025-07-18 12:23:17 +0000

    fix: imports

--- ulcerative_colitis/data/datasets/__init__.py
+++ ulcerative_colitis/data/datasets/__init__.py
@@ -3,36 +3,10 @@ from ulcerative_colitis.data.datasets.embeddings import (
     EmbeddingsPredict,
     EmbeddingsSubset,
 )
-from ulcerative_colitis.data.datasets.nancy_high import (
-    NancyHighPredict,
-    NancyHighTest,
-    NancyHighTrain,
-)
-from ulcerative_colitis.data.datasets.nancy_low import (
-    NancyLowPredict,
-    NancyLowTest,
-    NancyLowTrain,
-)
-from ulcerative_colitis.data.datasets.neutrophils import (
-    NeutrophilsPredict,
-    NeutrophilsTest,
-    NeutrophilsTrain,
-)
-from ulcerative_colitis.data.datasets.ulcerative_colitis import UlcerativeColitisTrain
 
 
 __all__ = [
     "Embeddings",
     "EmbeddingsPredict",
     "EmbeddingsSubset",
-    "NancyHighPredict",
-    "NancyHighTest",
-    "NancyHighTrain",
-    "NancyLowPredict",
-    "NancyLowTest",
-    "NancyLowTrain",
-    "NeutrophilsPredict",
-    "NeutrophilsTest",
-    "NeutrophilsTrain",
-    "UlcerativeColitisTrain",
 ]
