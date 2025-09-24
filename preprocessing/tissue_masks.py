db8d2d401af45849b0e8d74673694bf07c59b0ee
Adam Kukučka <524905@mail.muni.cz>
2025-09-24 15:51:55 +0000

    feat: tissue mask refactor

---
+++ preprocessing/tissue_masks.py
@@ -0,0 +1,65 @@
+from pathlib import Path
+from typing import cast
+
+import hydra
+import pyvips
+import ray
+from lightning.pytorch.loggers import Logger
+from mlflow.tracking import MlflowClient
+from omegaconf import DictConfig
+from openslide import OpenSlide
+from rationai.masks import (
+    process_items,
+    slide_resolution,
+    tissue_mask,
+    write_big_tiff,
+)
+from rationai.mlkit.autolog import autolog
+from rationai.mlkit.lightning.loggers import MLFlowLogger
+from ray._private.worker import RemoteFunction0
+
+
+def process_slide(slide_path: Path, level: int, output_path: Path) -> None:
+    with OpenSlide(slide_path) as slide:
+        mpp_x, mpp_y = slide_resolution(slide, level=level)
+
+    slide = cast("pyvips.Image", pyvips.Image.new_from_file(slide_path, page=level))
+    mask = tissue_mask(slide, mpp=(mpp_x + mpp_y) / 2)
+    mask_path = output_path / slide_path.with_suffix(".tiff").name
+
+    write_big_tiff(mask, path=mask_path, mpp_x=mpp_x, mpp_y=mpp_y)
+
+
+def make_remote_process_slide(
+    level: int, output_path: Path
+) -> RemoteFunction0[None, Path]:
+    @ray.remote
+    def _process_slide(slide_path: Path) -> None:
+        return process_slide(slide_path, level, output_path)
+
+    return _process_slide
+
+
+@hydra.main(config_path="../../configs", config_name="tissue_masks", version_base=None)
+@autolog
+def main(config: DictConfig, logger: Logger | None = None) -> None:
+    assert logger is not None, "Need logger"
+    assert isinstance(logger, MLFlowLogger), "Need MLFlowLogger"
+    assert isinstance(logger.experiment, MlflowClient), "Need MlflowClient"
+    assert logger.run_id is not None, "Need run_id"
+
+    slides = Path(config.slides_folder).glob("*.tiff")
+    output_path = Path(config.output_path)
+    output_path.mkdir(parents=True, exist_ok=True)
+    process_items(
+        list(slides),
+        process_item=make_remote_process_slide(
+            level=config.level, output_path=output_path
+        ),
+    )
+
+    logger.experiment.log_artifacts(logger.run_id, str(output_path), "tissue_masks")
+
+
+if __name__ == "__main__":
+    main()
