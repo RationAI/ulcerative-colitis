a2f2377c16e6e5ae8fb055f380794f93c11c3107
Adam Kukučka <524905@mail.muni.cz>
2025-09-24 14:47:23 +0000

    feat: qc with better logging

--- preprocessing/quality_control.py
+++ preprocessing/quality_control.py
@@ -3,78 +3,76 @@ import tempfile
 from pathlib import Path
 from typing import Any
 
-import click
-import mlflow
-from aiohttp import ClientConnectionError, ClientSession, ClientTimeout
-
-from preprocessing.paths import (
-    BASE_FOLDER,
-    QC_MASKS_PATH,
-    SLIDES_PATH_FTN,
-    SLIDES_PATH_IKEM,
-)
-
-
-REQUEST_LIMIT = 4
-REQUEST_TIMEOUT = 30 * 60  # 30 minutes
-MAX_REQUEST_RETRY_ATTEMPTS = 5
-BACKOFF_BASE = 2  # 2 seconds
-
-URL = "http://rayservice-qc-serve-svc.rationai-jobs-ns.svc.cluster.local:8000/"
-
-EXPERIMENT_NAME = "Ulcerative Colitis"
-
-
-semaphore = asyncio.Semaphore(REQUEST_LIMIT)
+import hydra
+from aiohttp import ClientSession, ClientTimeout
+from lightning.pytorch.loggers import Logger
+from mlflow.tracking import MlflowClient
+from omegaconf import DictConfig
+from rationai.mlkit.autolog import autolog
+from rationai.mlkit.lightning.loggers import MLFlowLogger
 
 
 async def put_request(
-    session: ClientSession, url: str, data: dict[str, Any]
+    session: ClientSession,
+    data: dict[str, Any],
+    semaphore: asyncio.Semaphore,
+    config: DictConfig,
 ) -> tuple[int, str]:
-    timeout = ClientTimeout(total=REQUEST_TIMEOUT)
+    timeout = ClientTimeout(total=config.request_timeout)
 
     try:
-        async with semaphore, session.put(url, json=data, timeout=timeout) as response:
+        async with (
+            semaphore,
+            session.put(config.url, json=data, timeout=timeout) as response,
+        ):
             text = await response.text()
 
             return response.status, text
     except TimeoutError:
         print(
-            f"Failed to process {data['wsi_path']}:\n\tTimeout after {REQUEST_TIMEOUT} seconds\n"
+            f"Failed to process {data['wsi_path']}:\n\tTimeout after {config.request_timeout} seconds\n"
         )
 
         return -1, "Timeout"
 
-    except ClientConnectionError:
-        print(f"Failed to process {data['wsi_path']}:\n\tConnection error\n")
 
-        return -1, "Connection error"
+def artifacts(wsi_stem: str) -> list[str]:
+    return [
+        f"{wsi_stem}.csv",
+        f"FoldingFunction_folding_test_{wsi_stem}.tiff",
+        f"Piqe_focus_score_piqe_median_{wsi_stem}.tiff",
+        f"Piqe_piqe_median_activity_mask_{wsi_stem}.tiff",
+        f"ResidualArtifactsAndCoverage_coverage_mask_{wsi_stem}.tiff",
+        f"ResidualArtifactsAndCoverage_cov_percent_heatmap_{wsi_stem}.tiff",
+    ]
 
 
 async def repeatable_put_request(
-    session: ClientSession, url: str, data: dict[str, Any], num_repeats: int
+    session: ClientSession,
+    data: dict[str, Any],
+    semaphore: asyncio.Semaphore,
+    config: DictConfig,
+    logger: MLFlowLogger,
 ) -> None:
-    for attempt in range(1, num_repeats + 1):
-        status, text = await put_request(session, url, data)
+    for attempt in range(1, config.num_repeats + 1):
+        status, text = await put_request(session, data, semaphore, config)
 
         if status == -1 and text == "Timeout":
             return
 
-        if status == -1 and text == "Connection error":
-            att_count = f"attempt {attempt}/{MAX_REQUEST_RETRY_ATTEMPTS}"
-            print(f"Connection error received for {data['wsi_path']} ({att_count})\n")
-            await asyncio.sleep(BACKOFF_BASE**attempt)
-
-            continue
-
         if status == 500 and text == "Internal Server Error":
-            att_count = f"attempt {attempt}/{MAX_REQUEST_RETRY_ATTEMPTS}"
+            att_count = f"attempt {attempt}/{config.num_repeats}"
             print(
                 f"Unexpected status 500 received for {data['wsi_path']} ({att_count}):\n\tResponse: {text}\n"
             )
-            await asyncio.sleep(BACKOFF_BASE**attempt)
+            await asyncio.sleep(2**attempt)
 
             continue
+
+        for artifact in artifacts(Path(data["wsi_path"]).stem):
+            logger.experiment.log_artifact(
+                logger.run_id, Path(data["output_path"]) / artifact, "qc_masks"
+            )
 
         print(
             f"Processed {data['wsi_path']}:\n\tStatus: {status} \n\tResponse: {text}\n"
@@ -86,13 +84,17 @@ async def repeatable_put_request(
 
 
 async def generate_report(
-    session: ClientSession, slides: list[Path], output_dir: str, save_location: str
+    session: ClientSession,
+    slides: list[Path],
+    save_location: str,
+    semaphore: asyncio.Semaphore,
+    config: DictConfig,
 ) -> None:
-    url = URL + "report"
+    url = config.connection_parameters.url + "report"
 
     data = {
         "backgrounds": [str(slide.resolve()) for slide in slides],
-        "mask_dir": output_dir,
+        "mask_dir": config.output_path,
         "save_location": save_location,
         "compute_metrics": True,
     }
@@ -106,24 +108,29 @@ async def generate_report(
 
 
 async def quality_control(
-    output_path: str, report_path: str, slides: list[Path]
+    report_path: str,
+    slides: list[Path],
+    semaphore: asyncio.Semaphore,
+    config: DictConfig,
+    logger: MLFlowLogger,
 ) -> None:
     async with ClientSession() as session:
         tasks = [
             repeatable_put_request(
                 session=session,
-                url=URL,
+                semaphore=semaphore,
+                config=config.connection_parameters,
+                logger=logger,
                 data={
-                    "wsi_path": str(slide.resolve()),
-                    "output_path": output_path,
-                    "mask_level": 3,
-                    "sample_level": 1,
-                    "check_residual": True,
-                    "check_folding": True,
-                    "check_focus": True,
-                    "wb_correction": True,
+                    "wsi_path": slide.as_posix(),
+                    "output_path": config.output_path,
+                    "mask_level": config.qc_parameters.mask_level,
+                    "sample_level": config.qc_parameters.sample_level,
+                    "check_residual": config.qc_parameters.check_residual,
+                    "check_folding": config.qc_parameters.check_folding,
+                    "check_focus": config.qc_parameters.check_focus,
+                    "wb_correction": config.qc_parameters.wb_correction,
                 },
-                num_repeats=MAX_REQUEST_RETRY_ATTEMPTS,
             )
             for slide in slides
         ]
@@ -135,45 +142,41 @@ async def quality_control(
         await generate_report(
             session=session,
             slides=slides,
-            output_dir=output_path,
             save_location=report_path,
+            semaphore=semaphore,
+            config=config,
         )
 
 
-@click.command()
-@click.option(
-    "--cohort",
-    type=click.Choice(["FTN", "IKEM"]),
-    required=True,
-    help="Cohort to process",
-)
-def main(cohort: str) -> None:
-    mlflow.set_experiment(experiment_name=EXPERIMENT_NAME)
+@hydra.main(config_path="../configs", config_name="quality_control", version_base=None)
+@autolog
+def main(config: DictConfig, logger: Logger | None = None) -> None:
+    assert logger is not None, "Need logger"
+    assert isinstance(logger, MLFlowLogger), "Need MLFlowLogger"
+    assert isinstance(logger.experiment, MlflowClient), "Need MlflowClient"
+    assert logger.run_id is not None, "Need run_id"
 
-    with (
-        mlflow.start_run(run_name=f"🎭 Quality Control Masks: {cohort}"),
-        tempfile.TemporaryDirectory(
-            prefix="qc_masks_report_", dir=Path(BASE_FOLDER).as_posix()
-        ) as tmp_dir,  # Create a temporary directory for the report
-    ):
+    semaphore = asyncio.Semaphore(config.request_limit)
+
+    Path(config.output_path).mkdir(parents=True, exist_ok=True)
+
+    with tempfile.TemporaryDirectory(
+        prefix="qc_masks_report_", dir=Path(config.paths.base_folder).as_posix()
+    ) as tmp_dir:
         report_path = Path(tmp_dir) / "report.html"
-        output_dir = QC_MASKS_PATH / cohort
-
-        output_dir.mkdir(parents=True, exist_ok=True)
-        report_path.parent.mkdir(parents=True, exist_ok=True)
-
-        slides_folder = SLIDES_PATH_FTN if cohort == "FTN" else SLIDES_PATH_IKEM
 
         asyncio.run(
             quality_control(
-                output_path=output_dir.absolute().as_posix(),
                 report_path=report_path.absolute().as_posix(),
-                slides=list(slides_folder.glob("*.tiff")),
+                slides=list(Path(config.slides_folder).glob("*.tiff")),
+                semaphore=semaphore,
+                config=config,
+                logger=logger,
             )
         )
 
-        mlflow.log_artifact(local_path=str(report_path), artifact_path="report")
-        mlflow.log_artifacts(local_dir=str(output_dir), artifact_path="qc_masks")
+        logger.experiment.log_artifact(logger.run_id, report_path)
+        logger.experiment.log_artifacts(logger.run_id, config.output_path, "qc_masks")
 
 
 if __name__ == "__main__":
