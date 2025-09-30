70de2f460b95846b9549b6075002867bf2bcd428
Adam Kukučka <xkukuck1@fi.muni.cz>
2025-09-30 17:20:38 +0000

    feat: repartition

--- preprocessing/tiling.py
+++ preprocessing/tiling.py
@@ -134,7 +134,7 @@ def tiling(df: pd.DataFrame, config: DictConfig) -> tuple[pd.DataFrame, pd.DataF
     )
 
     tiles = slides.flat_map(tile, num_cpus=0.2, memory=128 * 1024**2)
-    tiles = tiles.repartition(target_num_rows_per_block=128)
+    tiles = tiles.repartition(target_num_rows_per_block=512)
     tiles = tiles.map_batches(
         partial(tissue, tissue_folder=tissue_folder), num_cpus=0.2, memory=1024**3
     )
