f7026b3eb9f3fb6f7e9559d44a28202bb61395db
Adam Kukučka <xkukuck1@fi.muni.cz>
2026-01-25 17:21:45 +0000

    feat: tissue masks

---
+++ scripts/preprocessing/tissue_masks.py
@@ -0,0 +1,20 @@
+from kube_jobs import storage, submit_job
+
+
+COHORT = "ikem"  # "ikem", "ftn", or "knl_patos"
+
+submit_job(
+    job_name=f"ulcerative-colitis-dataset-creation-{COHORT.replace('_', '-')}",
+    username=...,
+    public=False,
+    cpu=32,
+    memory="128Gi",
+    script=[
+        "git clone https://gitlab.ics.muni.cz/rationai/digital-pathology/pathology/ulcerative-colitis.git workdir",
+        "cd workdir",
+        "git checkout feature/dataset",
+        "uv sync --frozen",
+        f"uv run -m preprocessing.tissue_masks +data=processed/{COHORT}",
+    ],
+    storage=[storage.secure.DATA],
+)
