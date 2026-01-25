727c729db7aaf31944805fd4c533a7c9869aef0c
Adam Kukučka <xkukuck1@fi.muni.cz>
2026-01-25 16:00:54 +0000

    feat: add scripts

---
+++ scripts/preprocessing/create_dataset.py
@@ -0,0 +1,20 @@
+from kube_jobs import storage, submit_job
+
+
+COHORT = "ikem"  # "ikem", "ftn", or "knl_patos"
+
+submit_job(
+    job_name=f"ulcerative-colitis-dataset-creation-{COHORT}",
+    username=...,
+    public=False,
+    cpu=2,
+    memory="4Gi",
+    script=[
+        "git clone https://gitlab.ics.muni.cz/rationai/digital-pathology/pathology/ulcerative-colitis.git workdir",
+        "cd workdir",
+        "git checkout feature/dataset",
+        "uv sync --frozen",
+        f"uv run -m preprocessing.create_dataset +data=raw/{COHORT}",
+    ],
+    storage=[storage.secure.DATA],
+)
