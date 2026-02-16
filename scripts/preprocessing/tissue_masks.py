c290bcad9784ea87473938a9dd26156c791cf3c8
Adam Kukučka <adam.kukucka4@gmail.com>
2026-02-16 17:39:09 +0100

    feat: tissue masks (#3)
    * chore: replace pdm with uv
    
    * feat: configs
    
    * feat: dataset creation
    
    * fix: configs
    
    * fix: configs
    
    * feat: add scripts
    
    * fix: invalid job name
    
    * fix: set slide_id as index and ensure nancy is an integer
    
    * chore: dependencies
    
    * feat: processed data
    
    * feat: tissue masks
    
    * feat: script
    
    * feat: script
    
    * fix: job name
    
    * fix: exclude .git folder from ray env
    
    * fix: exclude .venv folder from ray env
    
    * fix: typo
    
    * feat: job script
    
    * feat: config
    
    * feat: job script
    
    * fix: PR comments
    
    * chore: dependencies
    
    * feat: refactor configs
    
    * fix: PR comments
    
    * fix: typo
    
    * feat: update dataset uris
    
    * fix: PR
    
    * chore: mypy
    
    * feat: with ray init
    
    * feat: script
    
    ---------
    
    Co-authored-by: Adam Kukučka <xkukuck1@fi.muni.cz>

--- scripts/preprocessing/tissue_masks.py
+++ scripts/preprocessing/tissue_masks.py
@@ -2,16 +2,17 @@ from kube_jobs import storage, submit_job
 
 
 submit_job(
-    job_name="ulcerative-colitis-dataset-creation-...",
+    job_name="ulcerative-colitis-tissue-masks-...",
     username=...,
     public=False,
-    cpu=2,
-    memory="4Gi",
+    cpu=64,
+    memory="32Gi",
+    shm="16Gi",
     script=[
         "git clone https://github.com/RationAI/ulcerative-colitis.git workdir",
         "cd workdir",
         "uv sync --frozen",
-        "uv run -m preprocessing.create_dataset +dataset=raw/...",
+        "uv run --active -m preprocessing.tissue_masks +dataset=processed/...",
     ],
     storage=[storage.secure.DATA],
 )
