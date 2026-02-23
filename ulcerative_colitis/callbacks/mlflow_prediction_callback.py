674d3130fee9708723292bbe96d5149643a812a1
Adam Kukučka <xkukuck1@fi.muni.cz>
2026-02-23 18:34:07 +0000

    feat: prediction callback on test

--- ulcerative_colitis/callbacks/mlflow_prediction_callback.py
+++ ulcerative_colitis/callbacks/mlflow_prediction_callback.py
@@ -1,7 +1,11 @@
 from lightning import Callback, LightningModule, Trainer
 from rationai.mlkit.lightning.loggers.mlflow import MLFlowLogger
 
-from ulcerative_colitis.typing import Output, TileEmbeddingsPredictInput
+from ulcerative_colitis.typing import (
+    Output,
+    TileEmbeddingsInput,
+    TileEmbeddingsPredictInput,
+)
 
 
 class MLFlowPredictionCallback(Callback):
@@ -18,6 +22,24 @@ class MLFlowPredictionCallback(Callback):
         trainer.logger.log_table(
             {
                 "slide": [m["slide_name"] for m in batch[1]],
+                "prediction": outputs.tolist(),
+            },
+            artifact_file="predictions.json",
+        )
+
+    def on_test_batch_end(
+        self,
+        trainer: Trainer,
+        pl_module: LightningModule,
+        outputs: Output,
+        batch: TileEmbeddingsInput,
+        batch_idx: int,
+        dataloader_idx: int = 0,
+    ) -> None:
+        assert isinstance(trainer.logger, MLFlowLogger)
+        trainer.logger.log_table(
+            {
+                "slide": [m["slide_name"] for m in batch[2]],
                 "prediction": outputs.tolist(),
             },
             artifact_file="predictions.json",
