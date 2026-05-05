6f73eb94307cc99b8a5896995b67338e7f063e5c
Adam Kukučka <xkukuck1@fi.muni.cz>
2026-05-05 21:14:27 +0200

    refactor: labels

--- ml/data/datasets/labels.py
+++ ml/data/datasets/labels.py
@@ -1,8 +1,8 @@
 from enum import Enum
 from pathlib import Path
 
-import pandas as pd
 import torch
+from datasets import Dataset as HFDataset
 
 
 class LabelMode(Enum):
@@ -11,27 +11,27 @@ class LabelMode(Enum):
     NANCY_LOW = "nancy_low"
 
 
-def process_slides(slides: pd.DataFrame, mode: LabelMode | None = None) -> pd.DataFrame:
+def process_slides(slides: HFDataset, mode: LabelMode | None = None) -> HFDataset:
     match mode:
         case LabelMode.NEUTROPHILS:
-            slides["neutrophils"] = slides["nancy_index"] >= 2
+            slides = slides.map(lambda x: {"neutrophils": x["nancy_index"] >= 2})
         case LabelMode.NANCY_HIGH:
             # new labels: 0,1 -> 0; 2,3,4 -> 1,2,3
-            slides["nancy_index"] = slides["nancy_index"].apply(lambda x: max(0, x - 1))
+            slides = slides.map(lambda x: {"nancy_index": max(0, x["nancy_index"] - 1)})
         case LabelMode.NANCY_LOW:
             # new labels: 0,1 -> 0,1; 2,3,4 -> 2
-            slides["nancy_index"] = slides["nancy_index"].apply(lambda x: min(x, 2))
+            slides = slides.map(lambda x: {"nancy_index": min(x["nancy_index"], 2)})
 
-    slides["name"] = slides["path"].apply(lambda x: Path(x).stem)
+    slides = slides.map(lambda x: {"name": Path(x["path"]).stem})
     return slides
 
 
-def get_label(slide_metadata: pd.Series, mode: LabelMode) -> torch.Tensor:
+def get_label(slide_metadata: dict, mode: LabelMode) -> torch.Tensor:
     match mode:
         case LabelMode.NEUTROPHILS:
-            return torch.tensor(slide_metadata["neutrophils"].item()).float()
+            return torch.tensor(slide_metadata["neutrophils"]).float()
         case LabelMode.NANCY_HIGH | LabelMode.NANCY_LOW:
-            return torch.tensor(slide_metadata["nancy_index"].item()).long()
+            return torch.tensor(slide_metadata["nancy_index"]).long()
 
 
 def get_target_column(mode: LabelMode) -> str:
