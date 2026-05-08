e497a9375d46c7b7e9436ffa4ce8167b2b17ce82
Adam Kukučka <xkukuck1@fi.muni.cz>
2026-05-08 09:23:49 +0000

    feat: sampler WIP

---
+++ ml/data/samplers/auto_weighted_random_sampler.py
@@ -0,0 +1,26 @@
+from collections.abc import Sequence
+
+import pandas as pd
+from torch.utils.data import WeightedRandomSampler
+
+from ml.data.datasets import (
+    SlideEmbeddingsSubset,
+    TileEmbeddingsSubset,
+    TilesSubset,
+)
+
+
+class AutoWeightedRandomSampler(WeightedRandomSampler):
+    def __init__(
+        self,
+        dataset: TilesSubset | TileEmbeddingsSubset | SlideEmbeddingsSubset,
+        column: str,
+        replacement: bool = True,
+    ) -> None:
+        super().__init__(
+            self._get_weights(dataset.slides[column]), len(dataset), replacement
+        )
+
+    def _get_weights(self, column: pd.Series) -> Sequence[float]:
+        value_counts = column.value_counts()
+        return column.apply(lambda x: 1 / value_counts[x]).tolist()
