dcc83926f5b67952e06dd160c0709aa4ef04a77c
Adam Kukučka <xkukuck1@fi.muni.cz>
2026-08-17 16:54:48 +0000

    fix: comment out models_base_url in EmbedTiles initialization

--- preprocessing/embeddings_xai.py
+++ preprocessing/embeddings_xai.py
@@ -20,7 +20,6 @@ class EmbedTiles:
     def __init__(self, model: str, concurrency: int, pool_tokens: str) -> None:
         self.model = f"{model}/"
         self.client = AsyncClient(
-            # models_base_url="http://rayservice-model-fix-serve-svc.rationai-jobs-ns.svc.cluster.local:8000",
             limits=httpx.Limits(
                 max_connections=concurrency, max_keepalive_connections=concurrency
             ),
