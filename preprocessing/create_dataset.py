6527027a15696a8e71c32a1b5001ebff7b854eb2
Adam Kukučka <xkukuck1@fi.muni.cz>
2026-08-24 18:59:30 +0000

    feat: add drop_missing parameter to create_dataset function

--- preprocessing/create_dataset.py
+++ preprocessing/create_dataset.py
@@ -48,7 +48,11 @@ def get_slides(folder_path: Path, pattern: re.Pattern[str]) -> pd.DataFrame:
 
 
 def create_dataset(
-    folder: str, labels: list[str], institution: str, pattern: re.Pattern[str]
+    folder: str,
+    labels: list[str],
+    institution: str,
+    pattern: re.Pattern[str],
+    drop_missing: bool = True,
 ) -> tuple[pd.DataFrame, list[str], list[str]]:
     folder_path = Path(folder)
     labels_df = get_labels(folder_path, labels)
@@ -69,8 +73,9 @@ def create_dataset(
     missing_slides = dataset_df[dataset_df["path"].isna()][on].to_list()
 
     dataset_df = dataset_df[["case_id", "path", "nancy"]]
-    dataset_df = dataset_df.dropna()
-    dataset_df["nancy"] = dataset_df["nancy"].astype(int)
+    if drop_missing:
+        dataset_df = dataset_df.dropna()
+        dataset_df["nancy"] = dataset_df["nancy"].astype(int)
 
     return dataset_df, missing_slides, missing_labels
 
@@ -84,6 +89,7 @@ def main(config: DictConfig, logger: MLFlowLogger) -> None:
         config.dataset.labels,
         config.dataset.institution,
         re.compile(config.dataset.regex_pattern),
+        config.datset.drop_missing
     )
 
     with tempfile.TemporaryDirectory() as tmpdir:
